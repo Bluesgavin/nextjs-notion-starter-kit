25e20266696f8c4bf5e6aa4ee54b9ce459162880
GAVIN <41565350+Bluesgavin@users.noreply.github.com>
2024-07-28 11:42:00 +0800

    update: link

--- site.config.ts
+++ site.config.ts
@@ -50,16 +50,16 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'About',
+      pageId: 'cfd2ad3710d346629445d5dff8c14fd4'
+    },
+    {
+      title: 'Contact',
+      pageId: '483e663f4f454d369cfc131c9e8224f7'
+    }
+  ]
 })
